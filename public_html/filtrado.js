fd6ee03923a1af4248fd77bcd1cd64b042df6620
gaston <gaston@gaston-Aspire-7530>
2015-09-29 20:13:47 -0300

    Primer commit de comahue_NuestraUniversidad

---
+++ public_html/filtrado.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    if($("#TodasAreas").prop("checked")){
+       $("#Ciencias_Económicas").prop("cheked", true);
+       $("#Agronomia_Agricultura").prop("cheked", true);
+       $("#Humanidades_Ciencias_Sociales").prop("cheked", true);
+       $("#Idiomas_Extranjeros").prop("cheked", true);
+       $("#Informática").prop("cheked", true);
+       $("#Ciencias_Exactas_y_Naturales").prop("cheked", true);
+       $("#Pedagogía_Ciencias_de_la_Educación").prop("cheked", true);
+       $("#Salud").prop("cheked", true);
+       $("#Ingeniería_y_Carreras_Afines").prop("cheked", true);
+       $("#Comunicación_y_Derecho").prop("cheked", true);
+       $("#Turismo_Deporte_Medio_Ambiente").prop("cheked", true);
+    }
+    
+    //if($("#TodasDuraciones").is(":checked")){
+        
+    //}
+});
+
+
