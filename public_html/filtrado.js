15ac927c2999b4e9756d45a69225bbb62a9cf31e
gaston <gaston@gaston-Aspire-7530>
2015-10-30 15:43:41 -0300

    Agregado el filtro por areas

--- public_html/filtrado.js
+++ public_html/filtrado.js
@@ -1,21 +1,15 @@
-$(document).ready(function(){
-    if($("#TodasAreas").prop("checked")){
-       $("#Ciencias_Económicas").prop("cheked", true);
-       $("#Agronomia_Agricultura").prop("cheked", true);
-       $("#Humanidades_Ciencias_Sociales").prop("cheked", true);
-       $("#Idiomas_Extranjeros").prop("cheked", true);
-       $("#Informática").prop("cheked", true);
-       $("#Ciencias_Exactas_y_Naturales").prop("cheked", true);
-       $("#Pedagogía_Ciencias_de_la_Educación").prop("cheked", true);
-       $("#Salud").prop("cheked", true);
-       $("#Ingeniería_y_Carreras_Afines").prop("cheked", true);
-       $("#Comunicación_y_Derecho").prop("cheked", true);
-       $("#Turismo_Deporte_Medio_Ambiente").prop("cheked", true);
-    }
-    
-    //if($("#TodasDuraciones").is(":checked")){
-        
-    //}
-});
+      $(document).on("pageshow","#buscar_carrera_nombre" ,function(){
+        var variables=$(location).attr('search');
+        var arrVar=variables.split("=");
+        console.log(arrVar[1]);
+        console.log(variables);
+        if(arrVar[1]!="normal"){
+            $("#formularioBuscar").hide();
+            $("#divOfPs-input").val(arrVar[1]);
+            
+            $("#divOfPs-input").change();
+            
+        }                        
+        });
 
 
