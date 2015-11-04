1a990b3ff027af465a500b59bcba8ca1fd7abac7
gaston <gaston@gaston-Aspire-7530>
2015-11-04 13:14:51 -0300

    ulltimos retoque en ubuntu

--- public_html/filtrado.js
+++ public_html/filtrado.js
@@ -1,12 +1,11 @@
-      $(document).on("pageshow","#buscar_carrera_nombre" ,function(){
+      $(document).on("pagebeforeshow","#buscar_carrera_nombre" ,function(){
         var variables=$(location).attr('search');
         var arrVar=variables.split("=");
         console.log(arrVar[1]);
         console.log(variables);
         if(arrVar[1]!="normal"){
             $("#formularioBuscar").hide();
-            $("#divOfPs-input").val(arrVar[1]);
-            
+            $("#divOfPs-input").val(arrVar[1]);         
             $("#divOfPs-input").change();
             
         }                        
