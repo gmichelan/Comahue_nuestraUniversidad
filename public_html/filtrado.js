ea328b102ad09547ef520aac09f4d6f9fe074668
gaston <gaston@gaston-G551JW>
2015-12-11 12:48:56 -0300

    Ultima modificación

--- public_html/filtrado.js
+++ public_html/filtrado.js
@@ -1,16 +1,30 @@
 
-      $(document).on("pagebeforeshow","#buscar_carrera_nombre",function(){
-          
-        var variables=$(location).attr('search');
-        var arrVar=variables.split("=");
-        console.log(arrVar[1]);
-        console.log(variables);
-        if(arrVar[1]!="normal"){
-            $("#formularioBuscar").hide();
-            $("#divOfPs-input").val(arrVar[1]);         
-            $("#divOfPs-input").change();
-            
-        }                        
-        });
+//      $(document).on("pagebeforeshow","#buscar_carrera_nombre",function(){
+//          
+//        var variables=$(location).attr('search');
+//        var arrVar=variables.split("=");
+//        console.log(arrVar[1]);
+//        console.log(variables);
+//        if(arrVar[1]!="normal"){
+//            $("#formularioBuscar").hide();
+//            $("#divOfPs-input").val(arrVar[1]);         
+//            $("#divOfPs-input").change();
+//            
+//        }                        
+//        });
 
-
+function boton(valor){
+    if(valor=="normal"){
+        $("#divOfPs-input").val(""); 
+        
+        $("#formularioBuscar").show();     
+        $.mobile.changePage("#buscar_carrera_nombre", { transition: "slideup", changeHash: true, reload:true});
+        location.reload();
+    }
+    else{
+        $.mobile.changePage("#buscar_carrera_nombre", { transition: "slideup", changeHash: true});
+        $("#formularioBuscar").hide();
+        $("#divOfPs-input").val(valor); 
+        $("#divOfPs-input").change();   
+    }
+}
